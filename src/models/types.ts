54e2986ec652a4549dca7267cfd1a1f8d031145c
SARAH TRINDADE DOS SANTOS <a95717389@rn.senac.local>
2026-09-22 16:18:09 -0300

    adicionar tipos do projeto

---
+++ src/models/types.ts
@@ -0,0 +1,9 @@
+export type Perfil = 'ESTUDANTE' | 'ADMIN';
+export type StatusItem = 'PENDENTE' | 'EM_ANDAMENTO' | 'CONCLUIDO';
+export type StatusEstudo = 'PENDENTE' | 'CONCLUIDO' | 'NAO_REALIZADO';
+export interface UsuarioSessao {
+  id: number;
+  nome: string;
+  email: string;
+  perfil: Perfil;
+}
